581c335ad05a7a32d58f7dc9596f7a32e7cfec6e
and1er <34434784+and1er@users.noreply.github.com>
2021-02-08 10:01:14 +0300

    Explore pandas series data object.

--- series.py
+++ series.py
@@ -4,4 +4,8 @@ import pandas as pd
 
 if __name__ == '__main__':
     my_series = pd.Series([5, 6, 7, 8, 9, 10])
-    print(my_series)
+    print(f'String repr:\n{my_series}\n')
+    print(f'Indexes:\n{my_series.index}\n')
+    print(f'Values:\n{my_series.values}\n')
+    print(f'Get value by index:\n{my_series[4]}\n')
+    print(f'Get by index range:\n{my_series[2:5]}\n')
